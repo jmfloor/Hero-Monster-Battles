9886d8d513aa832800655c5a11891628fefe72b6
Johannes <33619163+jmfloor@users.noreply.github.com>
2018-02-02 11:40:32 +0200

    Build classes, properthies and methods for character class. staged one round of the battle and display the result

--- HeroMonsterBattles/Default.aspx
+++ HeroMonsterBattles/Default.aspx
@@ -9,6 +9,13 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h2>Hero vs Monster Battle</h2>
+            <p>
+                <asp:Label ID="statsLabel" runat="server"></asp:Label>
+            </p>
+            <p>
+                <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            </p>
         </div>
     </form>
 </body>
--- HeroMonsterBattles/Default.aspx.cs
+++ HeroMonsterBattles/Default.aspx.cs
@@ -11,7 +11,78 @@ namespace HeroMonsterBattles
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            character Hero = new character("Hero",100,50,10);
+            character Monster = new character("Monster", 100,50,10);
 
+            DisplayStats(Hero);
+            DisplayStats(Monster);
+            resultLabel.Text += "<br /> ========= <br />";
+
+
+            Monster.defend(Hero.attack());
+            Hero.defend(Monster.attack());
+
+
+            DisplayStats(Hero);
+            DisplayStats(Monster);
+        }
+
+        private void DisplayStats(character character)
+        {
+            string result = resultLabel.Text + "<br />";
+
+            result += character.Name + "<br />" +
+                "Health: " + character.Health + "<br />" +
+                "Damage Maximum: " + character.DamageMaximum + "<br />" +
+                "Attach Bonus: " + character.AttackBonus + "<br />";
+
+            resultLabel.Text = result;
+        }
+    }
+
+    class character
+    {
+        public string Name { get; set; }
+        public int Health  { get; set; }
+        public int DamageMaximum { get; set; }
+        public int AttackBonus { get; set; }
+
+        public character(string name, int health, int damageMaximum, int attackBonus)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Health = health;
+            DamageMaximum = damageMaximum;
+            AttackBonus = attackBonus;
+        }
+
+        public int attack()
+        {
+            Random random = new Random();
+            
+            return random.Next(this.DamageMaximum);
+
+        }
+
+        public int defend(int damage)
+        {
+            this.Health -= damage;
+            return this.Health;
+
+        }
+        public string showCharacterStats()
+        {
+            string result = "";
+            result += this.Name + "<br />" +
+                "Health: " + this.Health + "<br />" +
+                "Damage Maximum: " + this.DamageMaximum + "<br />" +
+                "Attach Bonus: " + this.AttackBonus + "<br />";
+
+            return result;
+        }
+
+        public string showAttack(int attackValue)
+        {
+            return "";   
         }
     }
 }
